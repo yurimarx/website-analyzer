686fa4cd8bb592b0843b7803dfc5398a36998a77
Yuri Gomes <yurimarx@gmail.com>
2020-12-20 21:48:30 -0300

    crawler operation

--- src/main/java/community/intersystems/websiteanalyzer/WebsiteAnalyzerOperation.java
+++ src/main/java/community/intersystems/websiteanalyzer/WebsiteAnalyzerOperation.java
@@ -1,16 +1,47 @@
 package community.intersystems.websiteanalyzer;
 
+import com.intersystems.enslib.pex.BusinessOperation;
+import com.intersystems.gateway.GatewayContext;
+import com.intersystems.jdbc.IRIS;
+import com.intersystems.jdbc.IRISObject;
+
+
 import edu.uci.ics.crawler4j.crawler.CrawlConfig;
 import edu.uci.ics.crawler4j.crawler.CrawlController;
 import edu.uci.ics.crawler4j.fetcher.PageFetcher;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
-public class CrawlerService {
 
-	public String execute(String website, Integer depth, Integer pages) {
-		final int MAX_CRAWL_DEPTH = depth;
-		final int NUMBER_OF_CRAWELRS = pages;
+public class WebsiteAnalyzerOperation extends BusinessOperation {
+
+	// Connection to InterSystems IRIS
+    private IRIS iris;
+
+	@Override
+	public void OnInit() throws Exception {
+		iris = GatewayContext.getIRIS();
+	}
+	
+	@Override
+	public Object OnMessage(Object request) throws Exception {
+		IRISObject req = (IRISObject) request;
+		
+		String website = req.getString("Website");
+		Long depth = req.getLong("Depth");
+		Long pages = req.getLong("Pages");
+		
+		String result = executeCrawler(website, depth, pages);
+		IRISObject response = (IRISObject)(iris.classMethodObject("Ens.StringContainer","%New", result));
+        
+		return response;
+	}
+	
+	public String executeCrawler(String website, Long depth, Long pages) {
+
+		final int MAX_CRAWL_DEPTH = depth.intValue();
+		final int NUMBER_OF_CRAWELRS = pages.intValue();
+		
 		final String OS = System.getProperty("os.name").toLowerCase();
 		
 		String CRAWL_STORAGE = "";
@@ -20,7 +51,7 @@ public class CrawlerService {
 		} else {
 			CRAWL_STORAGE = "/var/crawler/storage";
 		}
-		
+
 		CrawlConfig config = new CrawlConfig();
 		config.setCrawlStorageFolder(CRAWL_STORAGE);
 		config.setMaxDepthOfCrawling(MAX_CRAWL_DEPTH);
@@ -32,16 +63,22 @@ public class CrawlerService {
 		CrawlController controller;
 		try {
 			controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
 			controller.addSeed(website);
 
 			controller.start(CrawlerUtil.class, NUMBER_OF_CRAWELRS);
 			
-			return "success";
+			return "website extracted with success";
 
 		} catch (Exception e) {
 			return e.getMessage();
 		}
 
+	}
+
+	@Override
+	public void OnTearDown() throws Exception {
+		
 	}
 
 }
